5d1441cc6408fed04a59dd31007fd9d380dc1baa
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-08-06 23:06:56 +0200

    Add unit and integration tests to server
    Refactor server code to be more testable.
    Add test utils library.

--- test/test_server.py
+++ test/test_server.py
@@ -1,9 +1,17 @@
 import json
 import jsonschema
 import pytest
+import signal
+import zmq
 
+from mock import MagicMock, patch, call
+from test import utils
+
+from wolf import server
 from wolf.server import WolfServer
 from wolf.server import RequestException
+from wolf.server import setup_parser
+from wolf.server import execute_parser
 
 
 class MockParadigm(object):
@@ -35,3 +43,231 @@ class TestServerIntegration(object):
         request = json.dumps({'hehe': 'fail'})
         with pytest.raises(jsonschema.exceptions.ValidationError):
             self.server.handle_request(request)
+
+
+class TestServerArgparser(object):
+    def setup_method(self, method):
+        self.subparsers = MagicMock()
+
+    def test_setup_parser_sets_up_server_namespace(self):
+        ret = setup_parser(self.subparsers)
+        # Only check the first argument; a change to the help text should not
+        # make the test fail.
+        assert ret.add_parser.call_args[0][0] == 'server'
+
+    def test_setup_parser_sets_up_executor(self):
+        self.subparsers.executors = {}
+        ret = setup_parser(self.subparsers)
+        assert ret.executors == {
+            'server': execute_parser
+        }
+
+
+class TestServerArgparserSubcommandSetup(object):
+    def setup_method(self, method):
+        self.server_args = MagicMock()
+        self.sub_args = MagicMock()
+        self.server_args.add_subparsers.return_value = self.sub_args
+
+        self.subparsers = MagicMock()
+        self.subparsers.add_parser.return_value = self.server_args
+
+    def test_setup_parser_sets_up_start_command(self):
+        setup_parser(self.subparsers)
+        assert self.sub_args.add_parser.call_args_list[0][0][0] == 'start'
+
+    def test_setup_parser_sets_up_stop_command(self):
+        setup_parser(self.subparsers)
+        assert self.sub_args.add_parser.call_args_list[1][0][0] == 'stop'
+
+
+class TestServerArgparserIntegration(object):
+    def setup_method(self, method):
+        self.server = MagicMock()
+        self.ns = MagicMock()
+
+    @patch.object(server, 'WolfServer')
+    def test_execute_parser_start_server(self, WS):
+        self.ns.server_command = 'start'
+        self.WolfServer = WS
+        self.WolfServer.return_value = self.server
+        execute_parser(self.ns)
+
+        assert self.WolfServer.called
+        assert self.server.listen_forever.called
+
+    @patch('os.kill')
+    @patch('builtins.open')
+    def test_execute_parser_stop_server(self, op, kill):
+        pid = 9999
+        self.ns.server_command = 'stop'
+        op.return_value.read.return_value = str(pid)
+        ret = execute_parser(self.ns)
+
+        kill.assert_called_once_with(pid, signal.SIGINT)
+        assert ret == 0
+
+
+class TestServerSetupIntegration(object):
+    def setup_method(self, method):
+        self.server = WolfServer()
+        self.server.setup_paradigms = MagicMock()
+        self.server.setup_signals = MagicMock()
+        self.server.setup_pidfile = MagicMock()
+
+    def test_full_setup_calls_setup_paradigms(self):
+        self.server.setup()
+        assert self.server.setup_paradigms.called
+
+    def test_full_setup_calls_setup_signals(self):
+        self.server.setup()
+        assert self.server.setup_signals.called
+
+    def test_full_setup_calls_setup_pidfile(self):
+        self.server.setup()
+        assert self.server.setup_pidfile.called
+
+
+class TestServerSetupUnits(object):
+    def setup_method(self, method):
+        self.server = WolfServer()
+
+    def test_setup_paradigms(self):
+        pmock = MagicMock()
+        self.server.get_paradigms = MagicMock()
+        self.server.get_paradigms.return_value = {'mock': pmock}
+
+        self.server.setup_paradigms()
+        assert pmock.setup.called
+
+    @patch.object(signal, 'signal')
+    def test_setup_signals(self, signal):
+        self.server.setup_signals()
+
+        assert signal.call_args_list[0] == call(2, self.server.signal_handler)
+        assert signal.call_args_list[1] == call(15, self.server.signal_handler)
+
+    @patch('wolf.conf.PIDFILE')
+    @patch('os.getpid')
+    def test_setup_pidfile(self, getpid, pidfile):
+        pid = 13337
+        getpid.return_value = pid
+
+        op = utils.mock_open()
+        with patch('builtins.open', op):
+            self.server.setup_pidfile()
+
+        # This first one does not work for some reason. No matter; it's the
+        # second one that is important.
+        # assert op.called_once_with(pidfile)
+        assert op.return_value.write.called_once_with(str(pid))
+
+
+class TestServerSignalHandling(object):
+    def setup_method(self, method):
+        self.server = WolfServer()
+        self.server.teardown = MagicMock()
+
+    def test_signal_handler_calls_teardown(self):
+        self.server.signal_handler(2, MagicMock())
+        assert self.server.teardown.called
+
+
+class TestServerTeardownIntegration(object):
+    def setup_method(self, method):
+        self.server = WolfServer()
+        self.server.teardown_pidfile = MagicMock()
+
+    def test_teardown_when_not_torn_down(self):
+        assert not self.server.torn
+        self.server.teardown()
+
+        assert self.server.torn
+        assert self.server.teardown_pidfile.called
+
+    def test_teardown_when_already_torn_down(self):
+        self.server.torn = True
+        self.server.teardown()
+
+        assert self.server.torn
+        assert not self.server.teardown_pidfile.called
+
+
+class TestServerTeardownUnits(object):
+    def setup_method(self, method):
+        self.server = WolfServer()
+
+    @patch('wolf.conf.PIDFILE')
+    @patch('os.remove')
+    def test_teardown_pidfile(self, remove, pidfile):
+        self.server.teardown_pidfile()
+        assert remove.called_once_with(pidfile)
+
+
+class TestServerSocket(object):
+    def setup_method(self, method):
+        self.server = WolfServer()
+
+    @patch('zmq.Context')
+    def test_get_socket(self, Context):
+        port = 3307
+        ctx = MagicMock()
+        Context.return_value = ctx
+
+        ret = self.server.get_socket(port)
+        socket = ctx.socket()
+
+        assert ret is socket
+        assert socket.bind.called_once_with('tcp://*:{0}'.format(port))
+
+
+class TestServerSocketListener(object):
+    def setup_method(self, method):
+        self.server = WolfServer()
+        self.server.handle_request = MagicMock()
+        self.socket = MagicMock()
+
+    def test_socket_interruption(self):
+        self.socket.recv_unicode.side_effect = zmq.error.ZMQError
+
+        ret = self.server.listen(self.socket)
+        assert not ret, "Loop continued when it shouldn't"
+
+    def test_request_exception(self):
+        code, msg = 'TEST_ERROR', '#yolo'
+        self.server.handle_request.side_effect = RequestException(code, msg)
+
+        ret = self.server.listen(self.socket)
+        assert ret, "Loop halted because of exception when it shouldn't"
+        self.socket.send_unicode.called_once_with(
+            json.dumps({'code': code, 'message': msg})
+        )
+
+    def test_validation_exception(self):
+        msg = 'lel ur valiation suck'
+        exc = jsonschema.exceptions.ValidationError(msg)
+        self.server.handle_request.side_effect = exc
+
+        ret = self.server.listen(self.socket)
+        assert ret, "Loop halted because of exception when it shouldn't"
+        self.socket.send_unicode.called_once_with(
+            json.dumps({'code': 'VALIDATION_ERROR', 'message': msg})
+        )
+
+    def test_general_exception(self):
+        msg = 'random hax0r error :('
+        self.server.handle_request.side_effect = Exception(msg)
+
+        ret = self.server.listen(self.socket)
+        assert ret, "Loop halted because of exception when it shouldn't"
+        self.socket.send_unicode.called_once_with(
+            json.dumps({'code': 'UNKNOWN_ERROR', 'message': msg})
+        )
+
+
+class TestServerUtils(object):
+    @patch('wolf.conf.PIDFILE')
+    @patch('os.remove')
+    def test_is_running(self, remove, pidfile):
+        server.is_running()
+        assert remove.called_once_with(pidfile)
--- wolf/server.py
+++ wolf/server.py
@@ -15,7 +15,7 @@ class RequestException(Exception):
         self.code = code
         self.message = message
 
-    def __str__(self):
+    def __str__(self):  # pragma: nocover
         return "{0}: {1}".format(self.code, self.message)
 
 
@@ -27,95 +27,60 @@ class WolfServer(object):
         self.paradigms = {}
 
     def listen_forever(self, port=conf.PORT):  # pragma: nocover
+        """
+        Listen on the socket forever
+
+        This is pragma: nocover since it is by definition an infinite loop. All
+        the related units have their own tests.
+
+        """
+
         try:
-            context = zmq.Context()
-            socket = context.socket(zmq.REP)
+            socket = self.get_socket(port)
 
-            host = 'tcp://*:{0}'.format(port)
-            socket.bind(host)
-            self.log.info('Listening on {0}', host)
-
-            while True:
-                try:
-                    request_str = socket.recv_unicode()
-                except zmq.error.ZMQError as e:
-                    # This is for graceful exit of the socket
-                    self.log.info('Socket interrupted')
-                    return
-
-                try:
-                    response = self.handle_request(request_str)
-                except RequestException as e:
-                    response = {
-                        'code': e.code,
-                        'message': e.message,
-                    }
-                except jsonschema.exceptions.ValidationError as e:
-                    response = {
-                        'code': 'VALIDATION_ERROR',
-                        'message': e.message,
-                    }
-                except Exception as e:
-                    self.log.exception('Request handling exception')
-                    response = {
-                        'code': 'UNKNOWN_ERROR',
-                        'message': e.value,
-                    }
-
-                response_str = json.dumps(response)
-                socket.send_unicode(response_str)
-
-                # Separate blocks per request, for now
-                print()
+            # As long as listen returns positive, listen forever.
+            while self.listen(socket):
+                pass
 
         finally:
             # This usually means that the server died.
             self.teardown()
 
-    def setup(self):
-        self.log.info('Setting up server')
+    def listen(self, socket):
+        try:
+            # Listen on the socket; the actual wait is here
+            request_str = socket.recv_unicode()
+        except zmq.error.ZMQError as e:
+            # This is for graceful exit of the socket
+            self.log.info('Socket interrupted')
+            return False
 
-        self.paradigms = self.get_paradigms()
-        for paradigm in self.paradigms.values():
-            paradigm.setup()
+        try:
+            # Send the request for processing and handle any errors
+            response = self.handle_request(request_str)
+        except RequestException as e:
+            response = {
+                'code': e.code,
+                'message': e.message,
+            }
+        except jsonschema.exceptions.ValidationError as e:
+            response = {
+                'code': 'VALIDATION_ERROR',
+                'message': e.message,
+            }
+        except Exception as e:
+            self.log.exception('Request exception')
+            response = {
+                'code': 'UNKNOWN_ERROR',
+                'message': str(e),
+            }
 
-        # Set up signal handlers
-        def handle(signum, frame):
-            names = {2: 'SIGINT', 15: 'SIGTERM'}
-            self.log.warning('Recieved {0}', names[signum])
-            self.teardown()
+        # Dump as JSON string and send it back
+        response_str = json.dumps(response)
+        socket.send_unicode(response_str)
 
-        signal.signal(signal.SIGINT, handle)
-        signal.signal(signal.SIGTERM, handle)
-
-        # Set up pidfile
-        pid = str(os.getpid())
-        self.log.debug('Running on pid {0}', pid)
-        with open(conf.PIDFILE, 'w') as pidfile:
-            pidfile.write(pid)
-
-    def teardown(self):
-        if self.torn:
-            return
-
-        self.torn = True
-        self.log.info('Tearing down server')
-
-        self.log.debug('Removing pidfile')
-        os.remove(conf.PIDFILE)
-
-    def get_paradigms(self, *paradigms):
-        self.log.info('Loading paradigms')
-        return {
-            'shasum': ShasumParadigm()
-        }
-
-    def validate_request(self, request):
-        self.log.info('Validating request...')
-        validate(request, 'core')
-
-    def validate_response(self, response):
-        self.log.info('Validating response...')
+        # Return True to keep the listen_forever loop going
+        return True
 
     def handle_request(self, request_str):
         """
@@ -158,6 +123,67 @@ class WolfServer(object):
         self.log.debug('Returning response: {0}', response)
         return response
 
+    def setup(self):
+        self.log.info('Setting up server')
+        self.setup_paradigms()
+        self.setup_signals()
+        self.setup_pidfile()
+
+    def setup_paradigms(self):
+        self.paradigms = self.get_paradigms()
+        for paradigm in self.paradigms.values():
+            paradigm.setup()
+
+    def setup_signals(self):
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+
+    def setup_pidfile(self):
+        pid = str(os.getpid())
+        self.log.debug('Pid: {0}', pid)
+        with open(conf.PIDFILE, 'w') as pidfile:
+            pidfile.write(pid)
+
+    def signal_handler(self, signum, frame):
+        names = {2: 'SIGINT', 15: 'SIGTERM'}
+        self.log.warning('Recieved {0}', names[signum])
+        self.teardown()
+
+    def teardown(self):
+        if self.torn:
+            return
+
+        self.torn = True
+        self.log.info('Tearing down server')
+        self.teardown_pidfile()
+
+    def teardown_pidfile(self):
+        self.log.debug('Removing pidfile')
+        os.remove(conf.PIDFILE)
+
+    def get_socket(self, port):
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+
+        host = 'tcp://*:{0}'.format(port)
+        socket.bind(host)
+        self.log.info('Listening on {0}', host)
+
+        return socket
+
+    def get_paradigms(self):  # pragma: nocover
+        self.log.info('Loading paradigms')
+        return {
+            'shasum': ShasumParadigm()
+        }
+
+    def validate_request(self, request):
+        self.log.info('Validating request...')
+        validate(request, 'core')
+
+    def validate_response(self, response):
+        self.log.info('Validating response...')
+
 
 def setup_parser(subparsers):
     server = subparsers.add_parser(
@@ -173,6 +199,8 @@ def setup_parser(subparsers):
         'server': execute_parser
     })
 
+    return subparsers
+
 
 def execute_parser(ns):
     if ns.server_command == "stop":
@@ -183,7 +211,7 @@ def execute_parser(ns):
 
 
 def is_running():
-    # Use existance of the pidfile to determine if the server is running
+    # Use existence of the pidfile to determine if the server is running
     return os.path.isfile(conf.PIDFILE)
 
 
