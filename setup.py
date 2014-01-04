564e5aacc0aa99f639b0ba07f0dfa416241faa48
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-01-04 18:15:11 +0100

    Re-fix packaging
    I made a lot of poor decisions in October, heh.
    Also, bump year in license.

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
-from distutils.core import setup
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['test', '--cov=infect']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(
     name='kitten',
-    version='0.1-dev',
+    version='0.2.1',
     url='https://github.com/thiderman/kitten',
     author='Lowe Thiderman',
     author_email='lowe.thiderman@gmail.com',
     description=('A decentralized minimalist build system'),
     license='MIT',
     packages=['kitten'],
-    scripts=[
-        'bin/kitten'
-    ],
+    entry_points={
+        'console_scripts': [
+            'kitten = kitten:main',
+        ],
+    },
+    cmdclass={'test': PyTest},
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Console',
