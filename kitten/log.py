b042cbbde623bec1aa7134b1c710c1e83779925f
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-01-31 11:51:50 +0100

    Add milliseconds to logs

--- kitten/log.py
+++ kitten/log.py
@@ -3,4 +3,8 @@ from logbook import more
 
 def setup_color():  # pragma: nocover
     color = more.ColorizedStderrHandler()
+    color.format_string = '{0} {1}'.format(
+        '[{record.time:%Y-%m-%d %H:%M:%S.%f}]',
+        '{record.level_name}: {record.channel}: {record.message}',
+    )
     color.push_application()
