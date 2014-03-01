daa4565abe4059e8588ddf374fde0f51d9ec784e
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-03-01 12:06:55 +0100

    Create a skeleton for node propagation integration tests

---
+++ test/integration/test_node_propagation.py
@@ -0,0 +1,13 @@
+class TestPropagation(object):
+    def test_node_propagation(self):
+        """
+        Tests that check node propagation
+
+        1) Spin up four servers.
+        2) Make the first one send a sync request to all three others.
+        3) Count the numbers of requests made.
+        4) Check databases to see that they all know each other.
+
+        """
+
+        pass
