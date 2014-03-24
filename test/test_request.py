1cf9273752c7dd698018d66754b09d90c12deb35
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-03-24 22:49:52 +0100

    Refactor Process tests for phase model

--- test/test_request.py
+++ test/test_request.py
@@ -36,7 +36,7 @@ class TestRequestValidation(RequestMixin):
 class TestRequestProcess(RequestMixin):
     def setup_method(self, method):
         self.socket = MagicMock()
-        self.dry_request = {
+        self.request_payload = {
             'id': {
                 'uuid': 'uuid-hehe-etc',
                 'from': 'all of us',
@@ -47,21 +47,22 @@ class TestRequestProcess(RequestMixin):
             'paradigm': 'test',
             'method': 'test',
         }
-        self.request = KittenRequest(self.dry_request)
         super(TestRequestProcess, self).setup_method(method)
 
     def check_code(self, code, msg):
         check = {'code': code, 'message': msg}
-        check.update(self.dry_request)
+        check.update(self.request_payload)
         self.socket.send_json.assert_called_once_with(check)
 
     @patch.object(KittenRequest, 'process_confirm')
     @patch.object(KittenRequest, 'process_request_payload')
-    def test_process(self, pr, pc):
+    def test_process_request_payload(self, pr, pc):
+        request = KittenRequest(self.request_payload)
+
         response = MagicMock()
         pr.return_value = response
 
-        self.request.process(self.socket)
+        request.process(self.socket)
 
         pr.assert_called_once_with()
         self.socket.send_json.assert_called_once_with(response)
@@ -70,16 +71,19 @@ class TestRequestProcess(RequestMixin):
 
     @patch.object(KittenRequest, 'process_request_payload')
     def test_process_validationerror(self, pr):
+        request = KittenRequest(self.request_payload)
+
         msg = 'Hehehehee'
         pr.side_effect = jsonschema.exceptions.ValidationError(msg)
-        self.request.process(self.socket)
+        request.process(self.socket)
         self.check_code('VALIDATION_ERROR', msg)
 
     @patch.object(KittenRequest, 'process_request_payload')
     def test_process_exception(self, process_request_payload):
+        request = KittenRequest(self.request_payload)
         msg = '*giggles*'
         process_request_payload.side_effect = Exception(msg)
-        self.request.process(self.socket)
+        request.process(self.socket)
         self.check_code('UNKNOWN_ERROR', msg)
 
 
